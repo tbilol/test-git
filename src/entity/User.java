6252f42d930ae60e0414831cbcc35d0651743ce4
bilol <tbilol2908@gmail.com>
2025-05-15 16:15:55 +0500

    userda feildlar qoshildi

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,4 +1,49 @@
 package entity;
 
 public class User {
+    private String name;
+    private String surname;
+    private String email;
+    private Integer password;
+
+    public User(){}
+
+    public User(String name, String surname, String email, Integer password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPassword() {
+        return password;
+    }
+
+    public void setPassword(Integer password) {
+        this.password = password;
+    }
 }
