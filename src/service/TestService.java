bcb6aaf1e021e36dff24250f63b297be60ed2ef3
bilol <tbilol2908@gmail.com>
2025-05-15 16:38:18 +0500

    added check eamil method

--- src/service/TestService.java
+++ src/service/TestService.java
@@ -1,4 +1,10 @@
 package service;
 
 public class TestService {
+    public boolean checkEmail(String email){
+        if (email.matches("(A-Za-b1-9)@(a-z{6,})\\.(a-z{5})")) {
+            return true;
+        }
+        return false;
+    }
 }
